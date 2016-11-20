10d70caa52b77cdb787f3b23d4cf79890bddb177
Aaron Shafovaloff <aaronshaf@gmail.com>
2016-11-19 22:30:56 -0700

    updates

--- element.js
+++ element.js
@@ -2,7 +2,7 @@
 import preact from 'preact'
 import MyComponent from './components'
 
-export default class MyCustomElement extends HTMLElement {
+export default class MyCustomElement extends BabelHTMLElement {
   attributeChangedCallback(name, oldValue, newValue) {
     if (this.rendered) { this.updateRendering() }
   }
@@ -16,3 +16,11 @@ export default class MyCustomElement extends HTMLElement {
     this.rendered = true
   }
 }
+
+// https://github.com/w3c/webcomponents/issues/587#issuecomment-254126763
+function BabelHTMLElement() {
+  const newTarget = this.__proto__.constructor
+  return Reflect.construct(HTMLElement, [], newTarget)
+}
+Object.setPrototypeOf(BabelHTMLElement, HTMLElement)
+Object.setPrototypeOf(BabelHTMLElement.prototype, HTMLElement.prototype)
